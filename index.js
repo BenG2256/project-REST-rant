5985b9cd33140c71aa8194612d9451c271531e0e
Ben Gilley <bengilley5@gmail.com>
2023-10-23 13:19:44 -0400

    rest-rant pt1

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const app = express();
 
-app.use('/places', require('./controllers/places'))
+app.use("/places", require("./controllers/places"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page </h1>')
-})
+app.get("*", (req, res) => {
+  res.status(404).send("<h1>404 Page </h1>");
+});
 
 app.listen(process.emitWarning.PORT);
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log("app running on port 3000");
+});
