15b3b89cd8206fc61b012d2911ef61dcab69f3a9
Ben Gilley <bengilley5@gmail.com>
2023-10-25 18:51:00 -0400

    adding styles to Rest-rant

--- index.js
+++ index.js
@@ -2,13 +2,25 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+app.set('views', __dirname + '/views')
 app.set("view engine", "jsx");
 app.engine("jsx", require("express-react-views").createEngine());
+app.use(express.static('public'))
+
+
+
 app.use("/places", require("./controllers/places"));
 
 app.get("/", (req, res) => {
   res.render("home");
 });
+
+app.get('/', (req,res) => {
+  res.render('newplaces');
+})
+
+
+
 
 app.get("*", (req, res) => {
   res.render("error404");
