6be5d2231c706b136a6387845f29e8325d895229
Ben Gilley <bengilley5@gmail.com>
2023-10-25 15:13:41 -0400

    Adding home page view

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 require("dotenv").config();
 const express = require("express");
-
 const app = express();
 
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
 app.use("/places", require("./controllers/places"));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("home");
 });
 
 app.get("*", (req, res) => {
@@ -15,7 +16,7 @@ app.get("*", (req, res) => {
 
 app.listen(process.env.PORT);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3003;
 
 app.listen(PORT, () => {
   console.log(`app running on port ${PORT}`);
---
+++ views/home.jsx
@@ -0,0 +1,14 @@
+const React = require("react");
+const Def = require("./default");
+
+function home(html) {
+  return (
+    <Def>
+      <main>
+        <h1>HOME</h1>
+      </main>
+    </Def>
+  );
+}
+
+module.exports = home;
