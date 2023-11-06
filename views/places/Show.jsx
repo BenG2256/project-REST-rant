1301ce0c4a5c266db0ea20a320449a07050c6c66
Ben Gilley <bengilley5@gmail.com>
2023-11-06 17:24:51 -0500

    created and implimented helper method

--- views/places/Show.jsx
+++ views/places/Show.jsx
@@ -7,15 +7,19 @@ function show (data) {
           <main>
             <h1>{ data.place.name }</h1>
             <h2> Rating </h2>
-                <p>Not Rate</p>
-            <h2> Description</h2>
-            <p className="text-center">
-          Located in {data.place.city}, {data.place.state}</p>
-          </main>
+                <p>Not Rated</p>
           <body>
           <img src={data.place.pic} alt={data.place.name} />
-            <h2>Comments</h2>
-                <p>No comments yet!</p>
+          <h3>
+            Located in {data.place.city}, {data.place.state}
+          </h3>
+          <h2> Description</h2>
+          <h3>
+            {data.place.showEstablished()}
+          </h3>
+          <h4>
+            Serving {data.place.cuisines}
+          </h4>
           <a href={`/places/${data.id}/edit`} className='btn btn-warning'>
           Edit
           </a>
@@ -24,7 +28,10 @@ function show (data) {
                 Delete
             </button>
           </form>
+          <h2>Comments</h2>
+                <p>No comments yet!</p>
           </body>
+          </main>
         </Def>
     )
 }
