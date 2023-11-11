bb6c0fa74596339103cd2f3f55aaaa73928f2223
Ben Gilley <bengilley5@gmail.com>
2023-11-11 12:58:25 -0500

    star rating impleminted

--- views/places/Show.jsx
+++ views/places/Show.jsx
@@ -2,8 +2,26 @@ const React = require("react");
 const Def = require("../Default");
 
 function show(data) {
-  let comments = <h3 className="inactive">No Comments yet!</h3>;
+  let comments = (<h3 className="inactive">No Comments yet!</h3>)
+  let rating = (
+    <h3 className="inactive">
+      Not yet rated
+    </h3>
+  )
   if (data.place.comments.length) {
+    let sumRatings = data.place.comments.reduce((tot, c) => {
+      return tot + c.stars
+    }, 0)
+    let averageRating = Math.round(sumRatings / data.place.comments.length)
+    let stars = ''
+    for (let i = 0; i < averageRating; i++) {
+      stars += '⭐'
+    }
+    rating = (
+      <h3>
+       Rating: {stars} stars
+      </h3>
+    )
     comments = data.place.comments.map((c) => {
       return (
         <div className="border">
@@ -21,8 +39,8 @@ function show(data) {
     <Def>
       <main>
         <h1>{data.place.name}</h1>
-        <h2> Rating </h2>
-        <p>Not Rated</p>
+        <h2> {rating}</h2>
+       
         <body>
           <img src={data.place.pic} alt={data.place.name} />
           <h3>
